55897da8a75b41e4d9f331659cdeb6c4cb8a561d
Ried <f0009242@msu.edu>
2019-06-28 16:05:00 -0400

    Final Upload before confrences

--- ai.js
+++ ai.js
@@ -2,6 +2,10 @@ var Daniel = {
     name: 'AI',
     logic: (game) => {
         
+        function print(x) {
+            console.log(x)
+        }
+
         // Always prevent player to win
         console.log("hello")
         //Checks rows
@@ -9,57 +13,73 @@ var Daniel = {
         function checkRows() {
             var player_moves = 0
             var pos = 0
-            var possible_moves = null
+            var possible_move = null
             var possible_row = null
-            for(var x; x <= 2; x++) {
+            for(var x = 0; x <= 2; x++) {
                 player_moves = 0
                 pos = 0
                 possible_moves = null
                 possible_rows = null
-                for(var c; x <= 2; x++) {
+                for(var c = 0; c <= 2; c++) {
                     pos = c + 3*x
                     if(game.state[pos] == 'X') {
                         player_moves++
                         possible_row = x
                     }
+                    if(game.state[pos] == '') {
+                        possible_move = pos
+                    }
+                }
+
+                if (player_moves == 2 && possible_move != null){
+                    return(possible_move)
+
                 }
             }
+            console.log('finished checkRows')
+            return
         }
-        // for(var row = 0; row <= 2; row++) {
-        //     player_moves = 0
-        //     pos = 0
-        //     possible_moves = null
-        //     possible_rows = 0
-        //     move_made = false
-        //     for(var space = 0; space <= 2; space++){
-        //         pos = space + 3*row
-        //         if(game.state[pos] == "X") {
-        //             player_moves++
-        //             possible_rows = row
-        //         } else if(game.state[pos] == "") {
-        //             possible_moves = pos
-        //         }
-                
-        //         if(player_moves == 2) {
-        //             if(possible_moves == null) {
-        //                 for(var get = 0; get <=2; get++) {
-        //                     pos = get + 3*row
-        //                     if(game.state[pos] == ''){
-        //                         game.makeMove(pos)
-        //                         return;
-        //                     }
-        //                 }
-        //             }
 
-        //             if(possible_moves == null) {
-                        
-        //             }
-        //             game.makeMove(possible_moves)
-        //             // move_made = true
-        //             return;
-        //         }
-        //     }
-        // }
+        function checkDiagonal() {
+            var d1 = null
+            var d2 = null
+            var pos1 = null
+            var pos2 = null
+            if(game.state[0] == 'X') {
+                d1++
+            } else if (game.state[0] == '') {
+                pos1 = 0
+            }
+            if(game.state[2] == 'X') {
+                d2++
+            } else if (game.state[2] == '') {
+                pos2 = 2
+            }
+            if(game.state[4] == 'X') {
+                d1++
+                d2++
+            } else if (game.state[4] == '') {
+                pos1 = 4
+                pos2 = 4
+            }
+            if(game.state[6] == 'X'){
+                d2++
+            } else if (game.state[6] == '') {
+                pos2 = 6
+            }
+            if(game.state[8] == 'X'){
+                d1++
+            } else if (game.state[8] == '') {
+                pos1 = 8
+            }
+
+            if(d1 == 2) {
+                return(pos1)
+            } else if (d2 == 2) {
+                return(pos2)
+            }
+
+        }
 
         function checkColumns(){
             var player_moves = 0
@@ -67,8 +87,8 @@ var Daniel = {
             var possible_moves = null
             var posArray = [0,3,6]
             var possible_columns = null
-            for(var x; x <= 2; x++){
-                for(var c; c <= 2; c ++){
+            for(var x = 0; x <= 2; x++){
+                for(var c = 0; c <= 2; c ++){
                     pos = posArray[c] + x
                     if(game.state[pos == 'X']) {
                         player_moves++
@@ -79,48 +99,69 @@ var Daniel = {
                     
                 }
                 if(player_moves == 2 && possible_moves != null) {
-                    game.makeMove(possible_moves)
+                    return(possible_moves)
                 }
             }
+            console.log('finished check columns')
+            return
         }
 
-        console.log(possible_moves)
-        console.log(player_moves)
-        console.log(pos)
+        a = checkColumns()
+        b = checkRows()
+        c = checkDiagonal()
+        d = play_random_position()
 
+        if(a != null) {
+            game.makeMove(a)
+            return
+        }
+        if(b != null) {
+            game.makeMove(b)
+            return
+        }
+        if(c != null) {
+            game.makeMove(c)
+            return
+        }
+        if(d != null) {
+            game.makeMove(d)
+            return
+        }
         
 
         //Random AI
 
         function play_random_position() {
-            number = Math.random()
             played = false
-            if (number <= 0.1) {
-                position = 0
-            } else if (number <= 0.2) {
-                position = 1
-            } else if (number <= 0.3) {
-                position = 2
-            } else if (number <= 0.4) {
-                postion = 3
-            } else if (number <= 0.5) {
-                position = 4
-            } else if (number <= 0.6) {
-                position = 5
-            } else if (number <= 0.7) {
-                position = 6
-            } else if (number <= 0.8) {
-                postion = 7
-            } else if (number > 0.8) {
-                position = 8
-            }
+            
 
             while (!played) {
-            var x = random_position()
-            if(game.state[x] == ''){
-                game.makeMove(x)
-                played = true
+                var position
+                number = Math.random()
+                if (number <= 0.1) {
+                    position = 0
+                } else if (number <= 0.2) {
+                    position = 1
+                } else if (number <= 0.3) {
+                    position = 2
+                } else if (number <= 0.4) {
+                    position = 3
+                } else if (number <= 0.5) {
+                    position = 4
+                } else if (number <= 0.6) {
+                    position = 5
+                } else if (number <= 0.7) {
+                    position = 6
+                } else if (number <= 0.8) {
+                    position = 7
+                } else if (number > 0.8) {
+                    position = 8
+                }
+                if(game.state[position] == ''){
+                    played = true
+                    return position
             } 
+            
         }
 
         }
@@ -131,13 +172,13 @@ var Daniel = {
 
         // Original AI
 
-        if(game.state[6] == '') {
-            game.makeMove(6)                //if space 6 is open take space 6
-        } else if(game.state[7] == '') {
-            game.makeMove(7)                //if space 7 is open take space 7
-        } else if(game.state[8] == '') {
-            game.makeMove(8)                //if space 8 is open take space 8
-        }
+        // if(game.state[6] == '') {
+        //     game.makeMove(6)                //if space 6 is open take space 6
+        // } else if(game.state[7] == '') {
+        //     game.makeMove(7)                //if space 7 is open take space 7
+        // } else if(game.state[8] == '') {
+        //     game.makeMove(8)                //if space 8 is open take space 8
+        // }
         
     }
 }
